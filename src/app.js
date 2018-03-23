e2421bc5db430866d5b7ae1610a9daae4cde0d23
comid <461239254@qq.com>
2018-03-23 09:27:51 +0800

    初始化项目

---
+++ src/app.js
@@ -0,0 +1,6 @@
+/**
+ * 全局样式,全局方法，都写在此处
+ */
+
+import "./common/style/base.less";
+
---
+++ src/view/index/info/index.jsx
@@ -0,0 +1,12 @@
+import "./style/info.less";
+
+export default class Content extends React.Component {
+	render() {
+		return (
+			<div className="info-box">
+				详细信息
+			</div>
+		);
+	}
+
+};
